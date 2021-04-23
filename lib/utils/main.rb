3c381e61d0348baddd9a730032e91b50af849d03
1c7 <guokrfans@gmail.com>
2021-04-24 02:21:33 +0800

    Add encrypt

--- lib/utils/main.rb
+++ lib/utils/main.rb
@@ -1,15 +1,10 @@
+require 'openssl'
+require "base64"
+
 # 参照 JS SDK 的 encrypt 函数
 # https://github.com/Authing/authing.js/blob/cf4757d09de3b44c3c3f4509ae8c8715c9f302a2/src/lib/utils.ts#L12
-
-# const JSEncrypt = require('jsencrypt/bin/jsencrypt');
-# 这个模块是干啥的？
-# https://www.npmjs.com/package/jsencrypt
-# https://github.com/travist/jsencrypt
-# 就是一个干 RSA 的
-
-# Ruby 里的
-# https://rubygems.org/gems/openssl-signature_algorithm
-
-def encrypt
-
+def encrypt(plainText, publicKey)
+	key = OpenSSL::PKey::RSA.new(public_key)
+	result = key.public_encrypt(plainText)
+	return Base64.encode64(result)
 end
